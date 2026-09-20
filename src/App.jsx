3425c9648d769f4b1455996687998f9a2af34cfe
aldosarabia30-eng <aldosarabia30@gmail.com>
2026-09-20 00:29:20 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>Envelope Budget App</h1>
+      <p>Ready to Assign: $0.00</p>
+    </div>
+  )
+}
